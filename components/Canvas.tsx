ac91e909e93bed95b786381989ab8c71667e7486
Dominic-N1 <vladimirndsm@gmail.com>
2022-11-08 23:50:48 +0700

    add Canvas

---
+++ components/Canvas.tsx
@@ -0,0 +1,23 @@
+import React, { useRef, useEffect } from "react";
+import { prototype } from "stream";
+import sort from "../utils/sort";
+
+interface CanvasProps {
+  name?: string;
+  width: number;
+  height: number;
+}
+
+const Canvas = (props: CanvasProps): JSX.Element => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas === null) return;
+    const context = canvas.getContext("2d");
+    if (context === null) return;
+    sort(canvas, context);
+  }, []);
+  return <canvas ref={canvasRef} {...props} />;
+};
+
+export default Canvas;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Canvas from "../components/Canvas";
 
 export default function Home() {
   return (
@@ -17,41 +18,74 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <ul className={styles.grid}>
+          <li className={styles.card}>
+            <p>Learn!</p>
+          </li>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <li className={styles.card}>
+            <p>Discover.</p>
+          </li>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <li className={styles.card}>
+            <p>Instantly.</p>
+          </li>
+          <li className={styles.card}>
+            <Canvas width="300" height="300">
+              <section className="myButtons">
+                <button className="btn" type="button" data-sort="bubble">
+                  Bubble Sort
+                </button>
+                <button className="btn" type="button" data-sort="bubbleBack">
+                  Bubble Sort Back
+                </button>
+                <button className="btn" type="button" data-sort="selection">
+                  Selection Sort
+                </button>
+                <button className="btn" type="button" data-sort="selectionBack">
+                  Selection Sort Back
+                </button>
+                <button className="btn" type="button" data-sort="insertion">
+                  Insertion Sort
+                </button>
+              </section>
+              {/* <section className="myRange">
+                <ul>
+                  <li>
+                    <label htmlFor="speed">Speed</label>
+                    <br />
+                    <input
+                      type="range"
+                      name="speed"
+                      id="speed"
+                      defaultValue="3"
+                      min="1"
+                      max="5"
+                      // onChange={setSpeed()}
+                    />
+                  </li>
+                  <li>
+                    <label htmlFor="amount">Amount</label>
+                    <br />
+                    <input
+                      type="range"
+                      name="amount"
+                      id="amount"
+                      defaultValue="15"
+                      min="3"
+                      max="100"
+                      // onChange="setAmount()"
+                    />
+                  </li>
+                </ul>
+              </section> */}
+            </Canvas>
+          </li>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -60,12 +94,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
